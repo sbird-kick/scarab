bc54bd52b906e2a622c04f50bfd2b9afc39cec57
deepmish <deepmish@node0.wordpress.datacntr-effcy-pg0.wisc.cloudlab.us>
2024-11-06 16:59:37 -0600

    + Hashtables to store CPU cycles

--- src/globals/global_vars.h
+++ src/globals/global_vars.h
@@ -96,6 +96,23 @@ extern void* voided_curr_inst_reg_reg_mov_ptr;
 extern void* voided_curr_inst_mem_reg_mov_ptr;
 extern void* voided_curr_inst_mem_mem_mov_ptr;
 
+// Inst1:mem->mem and Inst2:mem->mem
+// Inst1:mem->mem and Inst2: reg->reg
+// Inst1: mem->mem and Inst2: reg->mem/mem->reg
+// Inst1: reg->reg and Inst2: reg->reg
+// Inst1: reg->reg and Inst2: reg->mem/mem->reg
+// Inst1: reg->reg and Inst2: mem->mem
+// Inst1: reg->mem/mem->reg and Inst2: reg->mem/mem->reg
+
+// Hashtables to store CPU cycles consumed by <MOV, MOV> instruction tuples 
+extern void* voided_prev_mem_mem_curr_reg_reg_ptr;
+extern void* voided_prev_mem_mem_curr_reg_mem_ptr;
+extern void* voided_prev_mem_mem_curr_mem_reg_ptr;
+extern void* voided_prev_reg_reg_curr_reg_reg_ptr;
+extern void* voided_prev_reg_reg_curr_reg_mem_ptr;
+extern void* voided_prev_reg_reg_curr_mem_mem_ptr;
+extern void* voided_prev_reg_mem_curr_reg_mem_ptr;
+
 /**************************************************************************************/
 
 #endif /* #ifndef __GLOBAL_VARS_H__ */
